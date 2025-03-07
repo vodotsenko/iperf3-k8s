75ce635c1a12296ad258eaaf54956a99d9845e1d
vodotsenko <dietaar98@gmail.com>
2025-03-07 10:07:17 +0000

    minor fix desc

--- main/run_cpu_stressor_50.sh
+++ main/run_cpu_stressor_50.sh
@@ -1,8 +1,8 @@
 kubectl config use-context admin@talos-proxmox-$cluster
-#50%
 echo "нагрузка на кластер 50%"
 kubectl apply -f /home/user/cpu-stressor/cpu-stressor-deploy.yaml
-sleep 2
+sleep 4
+kubectl rollout status deployment/cpu-stressor-deployment -n iperf3
 cpu_stressor=`kubectl get pod -n iperf3 -l app=cpu-stressor -o jsonpath="{.items[0].metadata.name}"`
 until $(kubectl get pod $cpu_stressor -n iperf3 -o jsonpath='{.status.containerStatuses[0].ready}'); do
   echo "Waiting for ${cpu_stressor} to start..."
