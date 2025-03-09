62d9a4d47db14d315e7e30dc043892b6cb8a850f
vodotsenko <dietaar98@gmail.com>
2025-03-09 13:09:15 +0000

    all ready for table 1

--- nonistio/start_test_from_nonistio.sh
+++ nonistio/start_test_from_nonistio.sh
@@ -2,8 +2,8 @@
 #echo "kubectl exec -it $iperf3_client -n iperf3 -c iperf3 -- bash -c \"traceroute $iperf3_svc_ip -q 1\""
 
 kubectl config use-context admin@talos-proxmox-$cluster &> /dev/null
-start_test_1P="kubectl exec -it ${iperf3_client} -n iperf3 -c iperf3 -- bash -c \"iperf3 -c ${iperf3_svc_ip} -t 60\""
-start_test_2P="kubectl exec -it ${iperf3_client} -n iperf3 -c iperf3 -- bash -c \"iperf3 -c ${iperf3_svc_ip} -t 60 -P 2\""
+start_test_1P="kubectl exec -it ${iperf3_client} -n iperf3 -c iperf3 -- bash -c \"iperf3 -c ${iperf3_svc_ip} -t 2\""
+start_test_2P="kubectl exec -it ${iperf3_client} -n iperf3 -c iperf3 -- bash -c \"iperf3 -c ${iperf3_svc_ip} -t 2 -P 2\""
 
 export start_test_1P start_test_2P cluster
 
