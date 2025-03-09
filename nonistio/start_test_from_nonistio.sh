c3931b97f178ff67795c92f1a415e27f0046aa21
vodotsenko <dietaar98@gmail.com>
2025-03-09 13:14:07 +0000

    test

--- nonistio/start_test_from_nonistio.sh
+++ nonistio/start_test_from_nonistio.sh
@@ -1,4 +1,4 @@
-#запустить клиента (nonistio)
+#запустить клиента (nonistio) 
 #echo "kubectl exec -it $iperf3_client -n iperf3 -c iperf3 -- bash -c \"traceroute $iperf3_svc_ip -q 1\""
 
 kubectl config use-context admin@talos-proxmox-$cluster &> /dev/null
